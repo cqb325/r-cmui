08becd543a442d54968b8a0f46e898df24795360
chenqingbiao <chenqingbiao@cmhi.chinamobile.com>
2017-10-12 14:27:21 +0800

    fixed tableform removeRow  & formcontrol onchange

--- build/config/paths.js
+++ build/config/paths.js
@@ -8,7 +8,7 @@
  * of patent rights can be found in the PATENTS file in the same directory.
  */
 // @remove-on-eject-end
-'use strict';
+
 
 const path = require('path');
 const fs = require('fs');
@@ -22,18 +22,18 @@ const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
 const envPublicUrl = process.env.PUBLIC_URL;
 
 function ensureSlash(path, needsSlash) {
-  const hasSlash = path.endsWith('/');
-  if (hasSlash && !needsSlash) {
-    return path.substr(path, path.length - 1);
-  } else if (!hasSlash && needsSlash) {
-    return `${path}/`;
-  } else {
-    return path;
-  }
+    const hasSlash = path.endsWith('/');
+    if (hasSlash && !needsSlash) {
+        return path.substr(path, path.length - 1);
+    } else if (!hasSlash && needsSlash) {
+        return `${path}/`;
+    } else {
+        return path;
+    }
 }
 
 const getPublicUrl = appPackageJson =>
-  envPublicUrl || require(appPackageJson).homepage;
+    envPublicUrl || require(appPackageJson).homepage;
 
 // We use `PUBLIC_URL` environment variable or "homepage" field to infer
 // "public path" at which the app is served.
@@ -42,28 +42,28 @@ const getPublicUrl = appPackageJson =>
 // We can't use a relative path in HTML because we don't want to load something
 // like /todos/42/static/js/bundle.7289d.js. We have to know the root.
 function getServedPath(appPackageJson) {
-  const publicUrl = getPublicUrl(appPackageJson);
-  const servedUrl =
+    const publicUrl = getPublicUrl(appPackageJson);
+    const servedUrl =
     envPublicUrl || (publicUrl ? url.parse(publicUrl).pathname : '/');
-  return ensureSlash(servedUrl, true);
+    return ensureSlash(servedUrl, true);
 }
 
 // config after eject: we're in ./config/
 module.exports = {
-  dotenv: resolveApp('.env'),
-  appBuild: resolveApp('build'),
-  appPublic: resolveApp('public'),
-  appHtml: resolveApp('public/index.html'),
-  appIndexJs: resolveApp('examples/index.js'),
-  appPackageJson: resolveApp('package.json'),
-  appSrc: resolveApp('src'),
-  appExample: resolveApp('examples'),
-  appLib: resolveApp('src/lib'),
-  yarnLockFile: resolveApp('yarn.lock'),
-  testsSetup: resolveApp('src/setupTests.js'),
-  appNodeModules: resolveApp('node_modules'),
-  publicUrl: getPublicUrl(resolveApp('package.json')),
-  servedPath: getServedPath(resolveApp('package.json')),
+    dotenv: resolveApp('.env'),
+    appBuild: resolveApp('dist'),
+    appPublic: resolveApp('public'),
+    appHtml: resolveApp('public/index.html'),
+    appIndexJs: resolveApp('examples/index.js'),
+    appPackageJson: resolveApp('package.json'),
+    appSrc: resolveApp('src'),
+    appExample: resolveApp('examples'),
+    appLib: resolveApp('src/lib'),
+    yarnLockFile: resolveApp('yarn.lock'),
+    testsSetup: resolveApp('src/setupTests.js'),
+    appNodeModules: resolveApp('node_modules'),
+    publicUrl: getPublicUrl(resolveApp('package.json')),
+    servedPath: getServedPath(resolveApp('package.json')),
 };
 
 // @remove-on-eject-begin
@@ -71,23 +71,23 @@ const resolveOwn = relativePath => path.resolve(__dirname, '..', relativePath);
 
 // config before eject: we're in ./node_modules/react-scripts/config/
 module.exports = {
-  dotenv: resolveApp('.env'),
-  appPath: resolveApp('.'),
-  appBuild: resolveApp('build'),
-  appPublic: resolveApp('public'),
-  appHtml: resolveApp('public/index.html'),
-  appIndexJs: resolveApp('examples/index.js'),
-  appPackageJson: resolveApp('package.json'),
-  appSrc: resolveApp('src'),
-   appExample: resolveApp('examples'),
-  yarnLockFile: resolveApp('yarn.lock'),
-  testsSetup: resolveApp('src/setupTests.js'),
-  appNodeModules: resolveApp('node_modules'),
-  publicUrl: getPublicUrl(resolveApp('package.json')),
-  servedPath: getServedPath(resolveApp('package.json')),
-  // These properties only exist before ejecting:
-  ownPath: resolveOwn('.'),
-  ownNodeModules: resolveOwn('node_modules'), // This is empty on npm 3
+    dotenv: resolveApp('.env'),
+    appPath: resolveApp('.'),
+    appBuild: resolveApp('dist'),
+    appPublic: resolveApp('public'),
+    appHtml: resolveApp('public/index.html'),
+    appIndexJs: resolveApp('examples/index.js'),
+    appPackageJson: resolveApp('package.json'),
+    appSrc: resolveApp('src'),
+    appExample: resolveApp('examples'),
+    yarnLockFile: resolveApp('yarn.lock'),
+    testsSetup: resolveApp('src/setupTests.js'),
+    appNodeModules: resolveApp('node_modules'),
+    publicUrl: getPublicUrl(resolveApp('package.json')),
+    servedPath: getServedPath(resolveApp('package.json')),
+    // These properties only exist before ejecting:
+    ownPath: resolveOwn('.'),
+    ownNodeModules: resolveOwn('node_modules'), // This is empty on npm 3
 };
 
 const ownPackageJson = require('../package.json');
@@ -98,27 +98,27 @@ const reactScriptsLinked =
 
 // config before publish: we're in ./packages/react-scripts/config/
 if (
-  !reactScriptsLinked &&
+    !reactScriptsLinked &&
   __dirname.indexOf(path.join('packages', 'react-scripts', 'config')) !== -1
 ) {
-  module.exports = {
-    dotenv: resolveOwn('template/.env'),
-    appPath: resolveApp('.'),
-    appBuild: resolveOwn('../../build'),
-    appPublic: resolveOwn('template/public'),
-    appHtml: resolveOwn('template/public/index.html'),
-    appIndexJs: resolveOwn('template/examples/index.js'),
-    appPackageJson: resolveOwn('package.json'),
-    appSrc: resolveOwn('template/src'),
-    appExample: resolveApp('examples'),
-    yarnLockFile: resolveOwn('template/yarn.lock'),
-    testsSetup: resolveOwn('template/src/setupTests.js'),
-    appNodeModules: resolveOwn('node_modules'),
-    publicUrl: getPublicUrl(resolveOwn('package.json')),
-    servedPath: getServedPath(resolveOwn('package.json')),
-    // These properties only exist before ejecting:
-    ownPath: resolveOwn('.'),
-    ownNodeModules: resolveOwn('node_modules'),
-  };
+    module.exports = {
+        dotenv: resolveOwn('template/.env'),
+        appPath: resolveApp('.'),
+        appBuild: resolveOwn('../../dist'),
+        appPublic: resolveOwn('template/public'),
+        appHtml: resolveOwn('template/public/index.html'),
+        appIndexJs: resolveOwn('template/examples/index.js'),
+        appPackageJson: resolveOwn('package.json'),
+        appSrc: resolveOwn('template/src'),
+        appExample: resolveApp('examples'),
+        yarnLockFile: resolveOwn('template/yarn.lock'),
+        testsSetup: resolveOwn('template/src/setupTests.js'),
+        appNodeModules: resolveOwn('node_modules'),
+        publicUrl: getPublicUrl(resolveOwn('package.json')),
+        servedPath: getServedPath(resolveOwn('package.json')),
+        // These properties only exist before ejecting:
+        ownPath: resolveOwn('.'),
+        ownNodeModules: resolveOwn('node_modules'),
+    };
 }
 // @remove-on-eject-end
