6fa4fa87925dd683bc0f030060ecbcaa98850c92
cqb325 <cqb325@163.com>
2017-11-13 21:58:24 +0800

    add gutter

--- src/components/Col/index.js
+++ src/components/Col/index.js
@@ -3,12 +3,20 @@ import classNames from 'classnames';
 import grids from '../utils/grids';
 
 class Col extends PureComponent{
+    static displayName = 'Col';
     render(){
         let className = classNames('cm-col', this.props.className, grids.getGrid(this.props.grid));
         let eleName = this.props.component || 'div';
+
+        let style = this.props.style || {};
+        if(this.props.gutter){
+            style.paddingLeft = this.props.gutter / 2;
+            style.paddingRight = this.props.gutter / 2;
+        }
+
         return React.createElement(eleName, {
             className: className,
-            style: this.props.style
+            style: style
         }, this.props.children);
     }
 }
--- src/components/Row/index.js
+++ src/components/Row/index.js
@@ -2,13 +2,32 @@ import React, {PureComponent} from 'react';
 import classNames from 'classnames';
 
 class Row extends PureComponent{
+    renderCols(){
+        return React.Children.map(this.props.children, (child)=>{
+            let componentName = child.type && child.type.displayName ? child.type.displayName : '';
+            if (componentName === 'Col') {
+                let props = Object.assign({
+                    'gutter': this.props.gutter
+                }, child.props);
+                return React.cloneElement(child, props);
+            } else {
+                return child;
+            }
+        });
+    }
+
     render(){
         let className = classNames('cm-row', this.props.className);
         let eleName = this.props.component || 'div';
+        let style = this.props.style || {};
+        if(this.props.gutter){
+            style.marginLeft = -this.props.gutter;
+            style.marginRight = -this.props.gutter;
+        }
         return React.createElement(eleName, {
             className: className,
-            style: this.props.style
-        }, this.props.children);
+            style: style
+        }, this.renderCols());
     }
 }
 
