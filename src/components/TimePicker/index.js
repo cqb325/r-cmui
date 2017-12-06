7db88dba783962fb601d229ed2485aeb6b32ac10
chenqingbiao <chenqingbiao@cmhi.chinamobile.com>
2017-12-06 16:48:20 +0800

    onchange

--- src/components/TimePicker/index.js
+++ src/components/TimePicker/index.js
@@ -10,7 +10,7 @@ import classNames from 'classnames';
 import Spinner from '../Spinner/index';
 import './TimePicker.less';
 
-class TimePicker extends BaseComponent{
+class TimePicker extends BaseComponent {
     static displayName = 'TimePicker';
     static defaultProps = {
         value: '00:00:00',
@@ -20,79 +20,118 @@ class TimePicker extends BaseComponent{
         format: 'HH:mm:ss'
     };
 
-    constructor(props){
+    constructor (props) {
         super(props);
 
         this.hasHour = props.format.indexOf('H') > -1;
         this.hasMinute = props.format.indexOf('m') > -1;
         this.hasSecond = props.format.indexOf('s') > -1;
 
-        let arr = props.value.split(':');
-        let time = moment();
+        const arr = props.value.split(':');
+        const time = moment();
         this.hasHour ? time.set('hour', parseInt(arr[0], 10)) : time.set('hour', 0);
         this.hasMinute ? time.set('minute', parseInt(arr[1], 10)) : time.set('minute', 0);
         this.hasSecond ? time.set('second', parseInt(arr[2], 10)) : time.set('second', 0);
         time.set('millisecond', 0);
 
-        let value = time.format(props.format);
+        const value = time.format(props.format);
 
         this.addState({
-            value: value,
+            value,
             current: time
         });
     }
 
-    plusHour = (v, step)=>{
+    plusHour = (v, step) => {
         this.timeChange(step, 'hour', 'plus');
     }
 
-    subHour = (v, step)=>{
+    subHour = (v, step) => {
         this.timeChange(-step, 'hour', 'sub');
     }
 
-    plusMinute = (v, step)=>{
+    changeHour = (hour) => {
+        const time = this.state.current;
+        const lastHour = time.get('hour');
+        if (lastHour == hour) {
+            return false;
+        }
+        const op = lastHour > hour ? 'plus' : 'sub';
+        const v = time.set('hour', hour).format(this.props.format);
+        this.setState({value: v, current: moment(time)}, () => {
+            this.emitChange(v, time, 'hour', op);
+        });
+    }
+
+    plusMinute = (v, step) => {
         this.timeChange(step, 'minute', 'plus');
     }
 
-    subMinute = (v, step)=>{
+    subMinute = (v, step) => {
         this.timeChange(-step, 'minute', 'sub');
     }
 
-    plusSecond = (v, step)=>{
+    changeMinute = (minute) => {
+        const time = this.state.current;
+        const lastMinute = time.get('minute');
+        if (lastMinute == minute) {
+            return false;
+        }
+        const op = lastMinute > minute ? 'plus' : 'sub';
+        const v = time.set('minute', minute).format(this.props.format);
+        this.setState({value: v, current: moment(time)}, () => {
+            this.emitChange(v, time, 'minute', op);
+        });
+    }
+
+    plusSecond = (v, step) => {
         this.timeChange(step, 'second', 'plus');
     }
 
-    subSecond = (v, step)=>{
+    subSecond = (v, step) => {
         this.timeChange(-step, 'second', 'sub');
     }
 
-    timeChange(step, type, op){
-        let time = this.state.current;
-        let v = time.add(step, type).format(this.props.format);
-        this.setState({value: v, current: moment(time)}, ()=>{
+    changeSecond = (second) => {
+        const time = this.state.current;
+        const lastSecond = time.get('second');
+        if (lastSecond == second) {
+            return false;
+        }
+        const op = lastSecond > second ? 'plus' : 'sub';
+        const v = time.set('second', second).format(this.props.format);
+        this.setState({value: v, current: moment(time)}, () => {
+            this.emitChange(v, time, 'second', op);
+        });
+    }
+
+    timeChange (step, type, op) {
+        const time = this.state.current;
+        const v = time.add(step, type).format(this.props.format);
+        this.setState({value: v, current: moment(time)}, () => {
             this.emitChange(v, time, type, op);
         });
     }
 
-    emitChange(v, time, type, op){
-        if(this.props.onChange){
+    emitChange (v, time, type, op) {
+        if (this.props.onChange) {
             this.props.onChange(v, time, type, op);
         }
 
         this.emit('change', v, time, type, op);
     }
 
-    getValue(){
+    getValue () {
         return this.state.value;
     }
 
-    getCurrent(){
+    getCurrent () {
         return this.state.current;
     }
 
-    setValue(value){
-        let arr = value.split(':');
-        let time = moment();
+    setValue (value) {
+        const arr = value.split(':');
+        const time = moment();
         arr[0] = arr[0] || 0;
         arr[1] = arr[1] || 0;
         arr[2] = arr[2] || 0;
@@ -103,43 +142,43 @@ class TimePicker extends BaseComponent{
         value = time.format(this.props.format);
 
         this.setState({
-            value: value,
+            value,
             current: time
         });
     }
 
     componentWillReceiveProps (nextProps) {
-        let value = nextProps.value;
+        const value = nextProps.value;
         if (value !== this.props.value && value !== this.state.value) {
             this.setValue(value);
         }
     }
 
-    renderTime(){
-        let time = this.state.current;
-        let hour = time.get('hour');
-        let minute = time.get('minute');
-        let second = time.get('second');
-        let h = this.hasHour
-            ? <Spinner loop size={this.props.size} value={hour} key="hour" max={23} step={this.props.hourStep}
-                onSub={this.subHour} onPlus={this.plusHour}
+    renderTime () {
+        const time = this.state.current;
+        const hour = time.get('hour');
+        const minute = time.get('minute');
+        const second = time.get('second');
+        const h = this.hasHour
+            ? <Spinner loop size={this.props.size} value={hour} key='hour' max={23} step={this.props.hourStep}
+                onSub={this.subHour} onPlus={this.plusHour} onChange={this.changeHour}
             />
             : null;
-        let m = this.hasMinute
-            ? <Spinner loop size={this.props.size} value={minute} key="munite" max={59} step={this.props.minuteStep}
-                onSub={this.subMinute} onPlus={this.plusMinute}
+        const m = this.hasMinute
+            ? <Spinner loop size={this.props.size} value={minute} key='munite' max={59} step={this.props.minuteStep}
+                onSub={this.subMinute} onPlus={this.plusMinute} onChange={this.changeMinute}
             />
             : null;
-        let s = this.hasSecond
-            ? <Spinner loop size={this.props.size} value={second} key="second" max={59} step={this.props.secondStep}
-                onSub={this.subSecond} onPlus={this.plusSecond}
+        const s = this.hasSecond
+            ? <Spinner loop size={this.props.size} value={second} key='second' max={59} step={this.props.secondStep}
+                onSub={this.subSecond} onPlus={this.plusSecond} onChange={this.changeSecond}
             />
             : null;
 
         return [h, m, s];
     }
 
-    render(){
+    render () {
         let {className, style} = this.props;
         className = classNames('cm-time-picker', className);
         return (
