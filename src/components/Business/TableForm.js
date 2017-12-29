f7d8f7935c14b1a03e3fead00e3146c9f3e9e41b
cqb325 <cqb325@163.com>
2017-12-29 15:00:16 +0800

    refs混乱问题

--- src/components/Business/TableForm.js
+++ src/components/Business/TableForm.js
@@ -4,8 +4,10 @@ import FormControl from '../FormControl/index';
 import Table from '../Table/index';
 import UUID from '../utils/UUID';
 
-class TableForm extends BaseComponent{
-    constructor(props) {
+class TableForm extends BaseComponent {
+    displayName = 'TableForm';
+    
+    constructor (props) {
         super(props);
 
         this.state = {
@@ -19,8 +21,8 @@ class TableForm extends BaseComponent{
      * 创建table使用的column
      * @param columns
      */
-    buildColumns(columns){
-        if(columns) {
+    buildColumns (columns) {
+        if (columns) {
             columns.forEach((column) => {
                 if (column.type) {
                     this.buildColumn(column);
@@ -37,8 +39,8 @@ class TableForm extends BaseComponent{
      * @param value
      * @param selectItem
      */
-    onChange(col, items, name, value, selectItem){
-        if(this.props.onChange){
+    onChange (col, items, name, value, selectItem) {
+        if (this.props.onChange) {
             this.props.onChange(name, value, col, items, selectItem);
         }
     }
@@ -47,18 +49,18 @@ class TableForm extends BaseComponent{
      * 创建列
      * @param column
      */
-    buildColumn(column){
-        let scope = this;
-        column.format = function(value, col){
-            let itemProps = Object.assign({}, column.props||{});
+    buildColumn (column) {
+        const scope = this;
+        column.format = (value, col, row) => {
+            const itemProps = Object.assign({}, column.props || {});
             scope.mergeProps(itemProps, column, ['rules','messages','name']);
             itemProps.value = value || itemProps.defaultValue || '';
-            return <FormControl ref={(ref)=>{
-                let refs = scope.getLastRefs();
-                if(refs) {
+            return <FormControl ref={(ref) => {
+                const refs = row.__rowItems;
+                if (refs) {
                     refs[column.name] = ref;
                 }
-            }} onChange={scope.onChange.bind(scope, col, scope.getLastRefs(), itemProps.name)} type={column.type} {...itemProps}/>;
+            }} onChange={scope.onChange.bind(scope, col, row.__rowItems, itemProps.name)} type={column.type} {...itemProps}/>;
         };
     }
 
@@ -68,10 +70,10 @@ class TableForm extends BaseComponent{
      * @param source
      * @param props
      */
-    mergeProps(target, source, props){
-        if(props){
-            props.forEach(function(prop){
-                if(source[prop] != undefined){
+    mergeProps (target, source, props) {
+        if (props) {
+            props.forEach((prop) => {
+                if (source[prop] != undefined) {
                     target[prop] = source[prop];
                 }
             });
@@ -82,10 +84,10 @@ class TableForm extends BaseComponent{
      * 获取最后的refs
      * @returns {*}
      */
-    getLastRefs(){
-        if(this.items.length) {
+    getLastRefs () {
+        if (this.items.length) {
             return this.items[this.items.length - 1];
-        }else{
+        } else {
             return null;
         }
     }
@@ -94,18 +96,18 @@ class TableForm extends BaseComponent{
      * 添加一行表格
      * @param row
      */
-    addRow(row){
-        let rowItems = {};
+    addRow (row) {
+        const rowItems = {};
         this.items.push(rowItems);
-        if(row) {
-            if(row.id == undefined){
+        if (row) {
+            if (row.id == undefined) {
                 row.id = UUID.v4();
             }
-            this.refs.table.addRow(row);
             row.__rowItems = rowItems;
-        }else{
+            this.refs.table.addRow(row);
+        } else {
             row = {};
-			this.props.columns.forEach((column)=>{
+            this.props.columns.forEach((column) => {
                 row[`${column.name}`] = column.defaultValue || '';
             });
             row.id = UUID.v4();
@@ -118,15 +120,15 @@ class TableForm extends BaseComponent{
      *
      * @param id
      */
-    getRowData(id){
-        let index = this.getIndexById(id);
+    getRowData (id) {
+        const index = this.getIndexById(id);
 
-        if(index != null){
-            if(index < this.items.length) {
-                let items = this.items[index];
-                let param = {};
-                for(let field in items){
-                    if(items[field].getValue) {
+        if (index != null) {
+            if (index < this.items.length) {
+                const items = this.items[index];
+                const param = {};
+                for (const field in items) {
+                    if (items[field].getValue) {
                         param[field] = items[field].getValue();
                     }
                 }
@@ -141,9 +143,9 @@ class TableForm extends BaseComponent{
      * 根据索引删除某一行
      * @param index
      */
-    removeRow(index){
+    removeRow (index) {
         this.refs.table.removeRow(index);
-        window.setTimeout(()=>{
+        window.setTimeout(() => {
             this.items.splice(index, 1);
         }, 0);
     }
@@ -152,9 +154,9 @@ class TableForm extends BaseComponent{
      *
      * @param id
      */
-    removeRowById(id){
-        let index = this.getIndexById(id);
-        if(index != null) {
+    removeRowById (id) {
+        const index = this.getIndexById(id);
+        if (index != null) {
             this.removeRow(index);
         }
     }
@@ -163,11 +165,11 @@ class TableForm extends BaseComponent{
      *
      * @param id
      */
-    getIndexById(id){
-        let tableData = this.refs.table.getData();
+    getIndexById (id) {
+        const tableData = this.refs.table.getData();
         let index = null;
-        for(let i = 0; i < tableData.length; i++){
-            if(tableData[i].key === id){
+        for (let i = 0; i < tableData.length; i++) {
+            if (tableData[i].key === id) {
                 index = i;
                 break;
             }
@@ -179,11 +181,11 @@ class TableForm extends BaseComponent{
     /**
      * 获取表格数据
      */
-    getData(){
-        return this.items.map((rowItems)=>{
-            let param = {};
-            for(let field in rowItems){
-                if(rowItems[field].getValue) {
+    getData () {
+        return this.items.map((rowItems) => {
+            const param = {};
+            for (const field in rowItems) {
+                if (rowItems[field].getValue) {
                     param[field] = rowItems[field].getValue();
                 }
             }
@@ -194,12 +196,12 @@ class TableForm extends BaseComponent{
     /**
      * 验证是否通过
      */
-    isValid(){
+    isValid () {
         let valid = true;
-        for(let i =0; i < this.items.length; i++){
-            let rowItems = this.items[i];
-            for(let field in rowItems){
-                if(rowItems[field].isFormItem() && rowItems[field].check() == false) {
+        for (let i = 0; i < this.items.length; i++) {
+            const rowItems = this.items[i];
+            for (const field in rowItems) {
+                if (rowItems[field].isFormItem() && rowItems[field].check() == false) {
                     valid = false;
                     return valid;
                 }
@@ -209,11 +211,11 @@ class TableForm extends BaseComponent{
         return valid;
     }
 
-    render(){
-        let {columns, className, bordered, hover, striped} = this.props;
+    render () {
+        const {columns, className, bordered, hover, striped} = this.props;
         this.buildColumns(columns);
         return (
-            <Table ref="table" columns={columns} data={this.state.data} className={className} bordered={bordered} hover={hover} striped={striped}>
+            <Table ref='table' columns={columns} data={this.state.data} className={className} bordered={bordered} hover={hover} striped={striped}>
             </Table>
         );
     }
