6cafca33212ebd18152e6095295cf777311aa0bb
cqb325 <cqb325@163.com>
2018-01-05 13:53:14 +0800

    添加List组件

---
+++ src/components/List/index.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+import Item from './Item';
+import Spin from '../Spin';
+const {SVGSpin} = Spin;
+
+import './List.less';
+
+class Comp extends React.Component {
+    displayName = 'Comp';
+
+    static defaultProps = {
+        spinning: false
+    };
+
+    static propTypes = {
+        head: PropTypes.any,
+        data: PropTypes.object,
+        actions: PropTypes.array,
+        className: PropTypes.string,
+        style: PropTypes.object,
+        border: PropTypes.bool,
+        spinning: PropTypes.bool,
+        size: PropTypes.oneOf(['large', 'small'])
+    };
+
+    renderHead () {
+        if (this.props.head) {
+            return <div className='cm-list-head'>
+                {this.props.head}
+            </div>;
+        }
+        return null;
+    }
+
+    renderItems () {
+        if (this.props.data) {
+            return this.props.data.map((item) => {
+                return <Item key={item.id} data={item} actions={this.props.actions}/>;
+            });
+        } else {
+            return null;
+        }
+    }
+
+    render () {
+        const {className, style, border, size} = this.props;
+        const clazzName = classNames('cm-list', className, {
+            'cm-list-border': border,
+            [`cm-list-${size}`]: size
+        });
+        return (
+            <div className={clazzName} style={style}>
+                {this.renderHead()}
+                <SVGSpin spinning={this.props.spinning}>
+                    {this.renderItems()}
+                </SVGSpin>
+            </div>
+        );
+    }
+}
+export default Comp;
