1d2723752bd18a55405130e6a502187dbf163cc5
cqb325 <cqb325@163.com>
2018-01-15 18:26:40 +0800

    suggest

---
+++ examples/suggest/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import Suggest from '../../src/components/Suggest';
+import fetch from '../../src/components/utils/fetch';
+
+class Comp extends React.Component {
+    displayName = 'Comp';
+
+    onFilter = async (v) => {
+        const ret = await fetch('http://172.18.34.66:8415/mock/ops-portal/monitor/metric/suggest');
+        const data = ret.data.map((item) => {
+            return {id: item, text: item};
+        });
+        this.suggest.setData(data);
+    }
+
+    componentDidMount () {
+        this.onFilter();
+    }
+
+    render () {
+        return (
+            <div>
+                <Suggest value='1' selectItems={{'1': 'asd'}} ref={(f) => this.suggest = f} filter onFilter={this.onFilter} placeholder='输入xxx'/>
+            </div>
+        );
+    }
+}
+export default Comp;
