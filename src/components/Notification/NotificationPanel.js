1464cad65a015cbeedc5a26997c6d3eb33369e72
cqb325 <cqb325@163.com>
2018-01-24 21:29:10 +0800

    notification修改

--- src/components/Notification/NotificationPanel.js
+++ src/components/Notification/NotificationPanel.js
@@ -4,76 +4,88 @@
  */
 
 import React, {PureComponent} from 'react';
-import ReactDOM from 'react-dom';
 import classNames from 'classnames';
-import velocity from '../../lib/velocity';
 
-class NotificationPanel extends PureComponent{
+class NotificationPanel extends PureComponent {
     static displayName = 'NotificationPanel';
+
+    state = {
+        open: false,
+        close: false
+    };
 
     /**
      * 关闭消息
      * @return {[type]} [description]
      */
-    close = ()=>{
+    close = () => {
         if (!this._isMounted) {
             return false;
         }
-        let ele = ReactDOM.findDOMNode(this);
-        velocity(ele, {height: 0, opacity: 0}, {
-            duration: 100,
-            easing: 'linear',
-            complete: ()=>{
-                this.props.parent.destroy(this.props.config.key);
-                if (this.props.config.onClose) {
-                    this.props.config.onClose();
-                }
-            }
+        this.setState({
+            close: true
         });
+        window.setTimeout(() => {
+            this.props.parent.destroy(this.props.config.key);
+            if (this.props.config.onClose) {
+                this.props.config.onClose();
+            }
+        }, 300);
     }
 
-    componentWillUnmount(){
+    componentWillUnmount () {
         this._isMounted = false;
     }
 
-    componentDidMount(){
+    componentDidMount () {
         this._isMounted = true;
-        let ele = ReactDOM.findDOMNode(this);
-        let params = {};
-        if (this.props.dock === 'topRight' || this.props.dock === 'bottomRight') {
-            params = {right: 0};
-        } else {
-            params = {left: 0};
-        }
-        velocity(ele, params, {duration: 100, easing: 'linear'});
+        // const ele = ReactDOM.findDOMNode(this);
+        // let params = {};
+        // if (this.props.config.dock === 'topRight' || this.props.config.dock === 'bottomRight') {
+        //     params = {right: 0};
+        // } else {
+        //     params = {left: 0};
+        // }
+        // velocity(ele, params, {duration: 100, easing: 'linear'});
 
+        setTimeout(() => {
+            this.setState({
+                open: true
+            });
+        },10);
+        
         if (this.props.config.duration) {
-            window.setTimeout(()=>{
+            window.setTimeout(() => {
                 this.close();
             }, this.props.config.duration * 1000);
         }
     }
 
-    render(){
-        let {className, style, icon, btn} = this.props.config;
+    render () {
+        let {className, style, icon, btn, theme} = this.props.config;
         className = classNames('cm-notification-item', className, {
-            'cm-notification-item-width-icon': icon
+            'cm-notification-item-width-icon': icon,
+            'cm-notification-item-open': this.state.open,
+            'cm-notification-item-close': this.state.close,
+            [`cm-notification-item-${theme}`]: theme
         });
         return (
             <div className={className} style={style}>
-                <a className="cm-notification-close" onClick={this.close}>x</a>
-                {
-                    icon ? <div className="cm-notification-icon">{icon}</div> : null
-                }
-                <div className="cm-notification-content">
-                    <div className="cm-notification-head">{this.props.config.title}</div>
-                    <div className="cm-notification-body">{this.props.config.desc}</div>
-                    { btn
-                        ? <span className="cm-notification-btn-wrap">
-                            {btn}
-                        </span>
-                        : null
+                <div className='cm-notification-item-wrap'>
+                    <a className='cm-notification-close' onClick={this.close}>x</a>
+                    {
+                        icon ? <div className='cm-notification-icon'>{icon}</div> : null
                     }
+                    <div className='cm-notification-content'>
+                        <div className='cm-notification-head'>{this.props.config.title}</div>
+                        <div className='cm-notification-body'>{this.props.config.desc}</div>
+                        { btn
+                            ? <span className='cm-notification-btn-wrap'>
+                                {btn}
+                            </span>
+                            : null
+                        }
+                    </div>
                 </div>
             </div>
         );
