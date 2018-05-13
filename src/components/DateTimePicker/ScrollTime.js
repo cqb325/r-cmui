afeaf582b733d41d0c23a79d0b71731a9a858ee1
cqb <694174384@qq.com>
2018-05-13 22:48:03 +0800

    scrollDate & scrollTime

---
+++ src/components/DateTimePicker/ScrollTime.js
@@ -0,0 +1,343 @@
+/**
+ * @author cqb 2018-05-13.
+ * @module ScrollTime
+ */
+import React from 'react';
+import classNames from 'classnames';
+import moment from 'moment';
+import Scroller from './Scroller';
+import PropTypes from 'prop-types';
+
+class ScrollTime extends React.Component {
+    displayName = 'ScrollTime';
+
+    static defaultProps = {
+        value: '',
+        format: 'HH:mm:ss',
+        minuteOnly: false,
+        hourOnly: false,
+        startTime: '',
+        endTime: ''
+    };
+
+    static propTypes = {
+        value: PropTypes.string,
+        format: PropTypes.string,
+        minuteOnly: PropTypes.bool,
+        hourOnly: PropTypes.bool,
+        onChange: PropTypes.func,
+        startTime: PropTypes.any,
+        endTime: PropTypes.any
+    }
+
+    constructor (props) {
+        super(props);
+
+        this.state = {
+            value: this.props.value,
+            current: this.props.value ? moment(this.props.value) : moment(),
+            startTime: this.props.startTime,
+            endTime: this.props.endTime
+        };
+    }
+
+    renderHead () {
+        let arr = ['时', '分', '秒'];
+        if (this.props.hourOnly) {
+            arr = ['时'];
+        }
+        if (this.props.minuteOnly) {
+            arr = ['时', '分'];
+        }
+        return arr.map((item) => {
+            return <div key={item} className='cm-scroll-date-head-item'>{item}</div>;
+        });
+    }
+
+    changeHour = () => {
+        if (this.state.startTime || this.state.endTime) {
+            this.hour.setEdge(this.getMinHour(), this.getMaxHour(), () => {
+                if (this.minute) {
+                    this.minute.setEdge(this.getMinMinute(), this.getMaxMinute(), () => {
+                        if (this.second) {
+                            this.second.setEdge(this.getMinSecond(), this.getMaxSecond(), () => {
+                                this.update();
+                            });
+                        } else {
+                            this.update();
+                        }
+                    });
+                } else {
+                    this.update();
+                }
+            });
+        } else {
+            this.update();
+        }
+    }
+
+    changeMinute = () => {
+        if (this.state.startTime || this.state.endTime) {
+            if (this.second) {
+                const max = this.getMaxSecond();
+                this.second.setEdge(this.getMinSecond(), max, () => {
+                    this.update();
+                });
+            } else {
+                this.update();
+            }
+        } else {
+            this.update();
+        }
+    }
+
+    update = () => {
+        const hour = this.hour.getValue();
+        let minute = 0;
+        let second = 0;
+        if (this.minute) {
+            minute = this.minute.getValue();
+        }
+        if (this.second) {
+            second = this.second.getValue();
+        }
+
+        const current = moment().set('hour', hour).set('minute', minute).set('second', second);
+        const f = this.getFormat();
+        const value = current.format(f);
+        if (value !== this.state.value) {
+            this.setState({
+                value, current
+            }, () => {
+                if (this.props.onChange) {
+                    this.props.onChange(value);
+                }
+            });
+        }
+    }
+
+    getFormat () {
+        let f = this.props.format;
+        if (this.props.hourOnly) {
+            f = 'HH';
+        }
+        if (this.props.minuteOnly) {
+            f = 'HH:mm';
+        }
+        return f;
+    }
+
+    getMaxHour () {
+        if (this.state.endTime) {
+            const end = moment(this.state.endTime, this.getFormat()).get('hour');
+            return end;
+        } else {
+            return 23;
+        }
+    }
+
+    getMinHour () {
+        if (this.state.startTime) {
+            return moment(this.state.startTime, this.getFormat()).get('hour');
+        } else {
+            return 0;
+        }
+    }
+
+    getMaxMinute () {
+        if (this.state.endTime) {
+            const end = moment(this.state.endTime, this.getFormat());
+            const hour = end.get('hour');
+            const current = this.state.current;
+            const chour = this.hour ? this.hour.getValue() : current.get('hour');
+            if (hour === chour) {
+                return end.get('minute');
+            } else {
+                return 59;
+            }
+        } else {
+            return 59;
+        }
+    }
+
+    getMinMinute () {
+        if (this.state.startTime) {
+            const end = moment(this.state.startTime, this.getFormat());
+            const hour = end.get('hour');
+            const current = this.state.current;
+            const chour = this.hour ? this.hour.getValue() : current.get('hour');
+            if (hour === chour) {
+                return end.get('minute');
+            } else {
+                return 0;
+            }
+        } else {
+            return 0;
+        }
+    }
+
+    getMaxSecond () {
+        if (this.state.endTime) {
+            const end = moment(this.state.endTime, this.getFormat());
+            const hour = this.hour ? this.hour.getValue() : this.state.current.get('hour');
+            const minute = this.minute ? this.minute.getValue() : this.state.current.get('minute');
+            const current = moment().set('hour', hour).set('minute', minute);
+            if (end.format('HH:mm') === current.format('HH:mm')) {
+                return end.get('second');
+            } else {
+                return 59;
+            }
+        } else {
+            return 59;
+        }
+    }
+
+    getMinSecond () {
+        if (this.state.startTime) {
+            const end = moment(this.state.startTime, this.getFormat());
+            const hour = this.hour ? this.hour.getValue() : this.state.current.get('hour');
+            const minute = this.minute ? this.minute.getValue() : this.state.current.get('minute');
+            const current = moment().set('hour', hour).set('minute', minute);
+            if (end.format('HH:mm') === current.format('HH:mm')) {
+                return end.get('second');
+            } else {
+                return 0;
+            }
+        } else {
+            return 0;
+        }
+    }
+
+    renderScrollers () {
+        const current = this.state.current;
+        const hour = current.get('hour');
+        const hourScroller = <Scroller ref={(f) => this.hour = f} key='hour' min={this.getMinHour()} max={this.getMaxHour()} value={hour} onChange={this.changeHour}/>;
+        const minute = current.get('minute');
+        const minuteScroller = <Scroller ref={(f) => this.minute = f} key='minute' min={this.getMinMinute()} max={this.getMaxMinute()} value={minute} onChange={this.changeMinute}/>;
+        const secondScroller = <Scroller ref={(f) => this.second = f} key='second' min={this.getMinSecond()} max={this.getMaxSecond()} value={current.get('second')} onChange={this.update}/>;
+
+        if (this.props.hourOnly) {
+            return hourScroller;
+        }
+
+        if (this.props.minuteOnly) {
+            return [hourScroller, minuteScroller];
+        }
+
+        return [hourScroller, minuteScroller, secondScroller];
+    }
+
+    getValue () {
+        return this.state.value;
+    }
+
+    setValue (value) {
+        if (value) {
+            this.setState({
+                value: value || '',
+                current: value ? moment(value, this.getFormat()) : moment()
+            });
+        }
+    }
+
+    /**
+     * 设置截止时间
+     * @param {*} endTime 
+     */
+    setEndTime (endTime) {
+        const current = this.state.current;
+        const end = moment(endTime, this.getFormat());
+        if (this.props.hourOnly) {
+            if (current.get('hour') > end.get('hour')) {
+                current.set('hour', end.get('hour'));
+            }
+        } else if (this.props.minuteOnly) {
+            if (current.format('HHmm') > end.format('HHmm')) {
+                current.set('hour', end.get('hour'));
+                current.set('minute', end.get('minute'));
+                if (this.minute) {
+                    this.minute.setMax(end.get('minute'));
+                }
+            }
+        } else {
+            if (current.format('HHmmss') > end.format('HHmmss')) {
+                current.set('hour', end.get('hour'));
+                current.set('minute', end.get('minute'));
+                current.set('second', end.get('second'));
+                if (this.minute) {
+                    this.minute.setMax(end.get('minute'));
+                }
+                if (this.second) {
+                    this.second.setMax(end.get('second'));
+                }
+            }
+        }
+        this.setState({
+            value: current.format(this.getFormat()),
+            current,
+            endTime
+        });
+    }
+
+    /**
+     * 设置起始时间
+     * @param {*} startTime 
+     */
+    setStartTime (startTime) {
+        const current = this.state.current;
+        const end = moment(startTime, this.getFormat());
+        if (this.props.hourOnly) {
+            if (current.get('hour') < end.get('hour')) {
+                current.set('hour', end.get('hour'));
+            }
+        } else if (this.props.minuteOnly) {
+            if (current.format('HHmm') < end.format('HHmm')) {
+                current.set('hour', end.get('hour'));
+                current.set('minute', end.get('minute'));
+                if (this.minute) {
+                    this.minute.setMin(end.get('minute'));
+                }
+            }
+        } else {
+            if (current.format('HHmmss') < end.format('HHmmss')) {
+                current.set('hour', end.get('hour'));
+                current.set('minute', end.get('minute'));
+                current.set('second', end.get('second'));
+                if (this.minute) {
+                    this.minute.setMin(end.get('minute'));
+                }
+                if (this.second) {
+                    this.second.setMin(end.get('second'));
+                }
+            }
+        }
+        this.setState({
+            value: current.format(this.getFormat()),
+            current,
+            startTime
+        });
+    }
+
+    componentWillReceiveProps (nextProps) {
+        const v = nextProps.value === 'undefined' ? '' : nextProps.value;
+        if (v !== this.props.value && v !== this.state.value) {
+            this.setValue(v);
+        }
+    }
+
+    render () {
+        const {className, style} = this.props;
+        const clazzName = classNames(className, 'cm-scroll-date');
+        return (
+            <div className={clazzName} style={style}>
+                <div className='cm-scroll-date-head cm-row'>
+                    {this.renderHead()}
+                </div>
+                <div className='cm-scroll-date-body'>
+                    {this.renderScrollers()}
+                </div>
+            </div>
+        );
+    }
+}
+export default ScrollTime;
---
+++ src/components/DateTimePicker/Scroller.js
@@ -0,0 +1,139 @@
+/**
+ * @author cqb 2018-05-12.
+ * @module Scroller
+ */
+
+import React from 'react';
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+
+class Scroller extends React.Component {
+    displayName = 'Scroller';
+
+    static defaultProps = {
+        min: 0,
+        max: 59,
+        value: 0
+    };
+
+    state = {
+        value: Math.max(this.props.value, this.props.min),
+        min: this.props.min,
+        max: this.props.max
+    };
+
+    renderItems () {
+        const arr = [];
+        let value = Math.max(this.state.value, this.state.min);
+        value = Math.min(value, this.state.max);
+        for (let i = this.state.min; i <= this.state.max; i++) {
+            const num = i < 10 ? `0${i}` : i;
+            const clazzName = classNames('cm-date-scroll-item-num', {
+                'cm-date-scroll-item-num-active': i === value
+            });
+            arr.push(<div className={clazzName} key={i}>{num}</div>);
+        }
+        return arr;
+    }
+
+    componentDidMount () {
+        this.wrap.addEventListener('mousewheel', this.scrollWrap, false);
+    }
+
+    scrollWrap = (e) => {
+        const delta = e.wheelDelta;
+        if (delta < 0) {
+            this.addNum();
+        } else {
+            this.subNum();
+        }
+    }
+
+    addNum () {
+        const value = Math.min(this.state.value + 1, this.state.max);
+        this.setState({value}, () => {
+            if (this.props.onChange) {
+                this.props.onChange(value);
+            }
+        });
+    }
+
+    subNum () {
+        const value = Math.max(this.state.value - 1, this.state.min);
+        this.setState({value}, () => {
+            if (this.props.onChange) {
+                this.props.onChange(value);
+            }
+        });
+    }
+
+    getValue () {
+        return this.state.value;
+    }
+
+    setValue (value) {
+        value = Math.max(value, this.state.min);
+        value = Math.min(value, this.state.max);
+        this.setState({value});
+    }
+
+    setMin (min, callback) {
+        const value = Math.max(this.state.value, min);
+        this.setState({
+            min,
+            value
+        }, () => {
+            callback ? callback() : false;
+        });
+    }
+
+    setMax (max, callback) {
+        const value = Math.min(this.state.value, max);
+        this.setState({max, value}, () => {
+            callback ? callback() : false;
+        });
+    }
+
+    setEdge (min, max, callback) {
+        let value = Math.max(this.state.value, min);
+        value = Math.min(value, max);
+        this.setState({min, max, value}, () => {
+            callback ? callback() : false;
+        });
+    }
+
+    componentWillUnmount () {
+        this.wrap.removeEventListener('mousewheel', this.scrollWrap);
+    }
+
+    componentWillReceiveProps (nextProps) {
+        if (nextProps.value !== this.props.value && nextProps.value !== this.state.value) {
+            this.setValue(nextProps.value);
+        }
+        if (nextProps.min !== this.props.min && nextProps.min !== this.state.min) {
+            this.setMin(nextProps.min);
+        }
+        if (nextProps.max !== this.props.max && nextProps.max !== this.state.max) {
+            this.setMax(nextProps.max);
+        }
+    }
+
+    render () {
+        const {className, style} = this.props;
+        const clazzName = classNames(className, 'cm-date-scroll-item');
+        let value = Math.max(this.state.value, this.state.min);
+        value = Math.min(value, this.state.max);
+        const top = -34 * (value - this.state.min);
+        const css = {
+            transform: `translate3d(0px, ${top}px, 0px)`
+        };
+        return (
+            <div className={clazzName} style={style} ref={(f) => this.wrap = f}>
+                <div className='cm-date-scroll-item-wrap' style={css}>
+                    {this.renderItems()}
+                </div>
+            </div>
+        );
+    }
+}
+export default Scroller;
