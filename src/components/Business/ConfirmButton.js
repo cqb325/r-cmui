a4dae49d86cc8bf7e6fef93acc1fa95254c21359
chenqingbiao <chenqingbiao@cmhi.chinamobile.com>
2018-08-13 14:44:42 +0800

    添加ConfirmButton&Form bug修改

---
+++ src/components/Business/ConfirmButton.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Button from '../Button';
+import MessageBox from '../MessageBox';
+
+class ConfirmButton extends React.Component {
+    displayName = 'ConfirmButton';
+
+    box = null;
+
+    onClick = () => {
+        this.tip.show(this.props.tip);
+    }
+
+    onConfirm = (flag) => {
+        if (flag) {
+            if (this.props.onConfirm) {
+                this.props.onConfirm(this.props.data);
+            }
+        }
+        return true;
+    }
+
+    render () {
+        return <span>
+            <Button {...this.props} onClick={this.onClick}>{this.props.children}</Button>
+            <MessageBox title='提示' ref={(f) => this.tip = f} type='confirm' confirm={this.onConfirm}/>
+        </span>;
+    }
+}
+
+export default ConfirmButton;
